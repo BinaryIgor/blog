dfa3a9850d9eeb878df96d5ccb1b76eeb3871c6e
Igor <iroztropinski@gmail.com>
2023-11-26 14:07:26 +0100

    better borders

--- pages/components.js
+++ pages/components.js
@@ -33,12 +33,13 @@ export function postHtmlDescription({ excerpt, htmlDescription }) {
 export function postPreview(post) {
     const postUrl = `${post.slug}.html`;
     return `
-   <li class="cursor-pointer border-[3px] border-dashed border-text-secondary-2 rounded-md p-6
+   <li class="cursor-pointer border-[3px] border-solid border-primary-text-faded p-6 rounded-sm
         max-content-width m-auto"
     onclick="location.href='${postUrl}'">
         <a href="${postUrl}" class="text-2xl mb-2 font-semibold">${post.title}</a>
         <div class="text-lg mb-6 text-secondary-3 flex flex-wrap justify-center whitespace-pre-wrap">${postMetadata(post)}</div>
         <div>${post.excerpt}</div>
+        
     </li>`;
 }
 
