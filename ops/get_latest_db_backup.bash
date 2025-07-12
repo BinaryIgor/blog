c584a90581787e84bfdaa27718e2e46a2dc45abc
Igor <igor@binaryigor.com>
2025-07-12 08:53:37 +0200

    Fix get_lastest_db_backup script

--- ops/get_latest_db_backup.bash
+++ ops/get_latest_db_backup.bash
@@ -5,7 +5,7 @@ source ci_config.env
 cd ..
 mkdir -p backups
 
-local_backup_path="backups/analytics_$(date +'%m%d%Y').db"
+local_backup_path="backups/analytics_$(date +'%Y%m%d').db"
 remote_backup_path="${ASSETS_DIR}/analytics_backup.db"
 
 echo "Getting backup from remote $remote_backup_path to local $local_backup_path..."
