3645a77ae978ae628da970222a5796219e3182ea
Igor <igor@binaryigor.com>
2025-12-19 07:49:57 +0100

    Performance tag & posts

--- src/server/config.js
+++ src/server/config.js
@@ -65,6 +65,7 @@ function analyticsAllowedPaths() {
         "/bootstrap.html",
         "/dbs-posts.html",
         "/modularity-posts.html",
+        "/performance-posts.html",
         "/htmx-posts.html",
         "/auth-posts.html",
         "/networks-posts.html",
